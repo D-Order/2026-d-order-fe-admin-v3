51141e0985092d80d7cad67c859957d9701a2319
nicerjs23 <nicerjs23@gmail.com>
2026-03-29 13:54:09 +0900

    Chore: 빌드오류 수정

--- src/pages/mypage/components/TableCountField.tsx
+++ src/pages/mypage/components/TableCountField.tsx
@@ -1,38 +1,57 @@
 // mypage/components/TableCountField.tsx
-import React from "react";
-import InfoRow from "./InfoRow";
-import * as S from "../MyPage.styled";
-import FieldActions from "./FieldActions";
+import InfoRow from './InfoRow';
+import * as S from '../MyPage.styled';
+import FieldActions from './FieldActions';
 
 type Props = {
-    editing: boolean;
-    value: number;
-    input: string;
-    setInput: (v: string) => void;
-    onEdit: () => void;
-    onConfirm: () => void;
-    onCancel: () => void;
+  editing: boolean;
+  value: number;
+  input: string;
+  setInput: (v: string) => void;
+  onEdit: () => void;
+  onConfirm: () => void;
+  onCancel: () => void;
 };
 
-const TableCountField = ({ editing, value, input, setInput, onEdit, onConfirm, onCancel }: Props) => {
-    return (
-        <InfoRow label="테이블 수">
-        {editing ? (
-            <S.NameInput
-            value={input}
-            onChange={(e) => setInput(e.target.value.replace(/[^0-9]/g, ""))} // 숫자만 입력 가능
-            placeholder="테이블 수 입력"
-            style={{ maxWidth: "6rem" }}
-            />
-        ) : (
-            <S.Value>{value ?? "-"}</S.Value>
-        )}
+const TableCountField = ({
+  editing,
+  value,
+  input,
+  setInput,
+  onEdit,
+  onConfirm,
+  onCancel,
+}: Props) => {
+  return (
+    <InfoRow label='테이블 수'>
+      {editing ? (
+        <S.NameInput
+          value={input}
+          onChange={(e) => setInput(e.target.value.replace(/[^0-9]/g, ''))} // 숫자만 입력 가능
+          placeholder='테이블 수 입력'
+          style={{ maxWidth: '6rem' }}
+        />
+      ) : (
+        <S.Value>{value ?? '-'}</S.Value>
+      )}
 
-        <div style={{ marginLeft: "auto", display: "flex", gap: "0.75rem", marginRight: "1.6875rem" }}>
-            <FieldActions editing={editing} onEdit={onEdit} onConfirm={onConfirm} onCancel={onCancel} />
-        </div>
-        </InfoRow>
-    );
+      <div
+        style={{
+          marginLeft: 'auto',
+          display: 'flex',
+          gap: '0.75rem',
+          marginRight: '1.6875rem',
+        }}
+      >
+        <FieldActions
+          editing={editing}
+          onEdit={onEdit}
+          onConfirm={onConfirm}
+          onCancel={onCancel}
+        />
+      </div>
+    </InfoRow>
+  );
 };
 
 export default TableCountField;
